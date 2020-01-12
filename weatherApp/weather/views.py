8b890087fc97f0ab5be5b7845570ff80e6ccdb43
MeshackKipkorir <meshackkipkorir105@gmail.com>
2020-01-12 18:43:11 -0500

    Added add city functionality

--- weatherApp/weather/views.py
+++ weatherApp/weather/views.py
@@ -4,9 +4,13 @@ from .models import weatherModel
 from .forms import CityForm
 # Create your views here.
 def weather_view(request,*args,**kwargs):
+    form = CityForm()
     if request.method == "POST":
-        pass
-    myform = CityForm()
+         form = CityForm(request.POST)
+    if form.is_valid():
+        form.save()
+        form = CityForm()
+        weatherModel.objects.create(**form.cleaned_data)
 
     url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=716806f2f07ef0d6d7a6bcec3c866551"
     weather_data = []
@@ -15,13 +19,14 @@ def weather_view(request,*args,**kwargs):
         result = requests.get(url.format(city)).json()
         city_weather = {
             "city":city.name,
-            # "description":result['weather'][0]['description'],
-            # "temparature":result['main']['temp'],
-            # "icon":result['weather'][0]['icon']
+            "description":result['weather'][0]['description'],
+            "temparature":result['main']['temp'],
+            "icon":result['weather'][0]['icon']
         }
         weather_data.append(city_weather)
         
     context = { 
-        "weather_data":weather_data
+        "weather_data":weather_data,
+        "myform":form
     }
     return render(request,'weather_update.html',context)
