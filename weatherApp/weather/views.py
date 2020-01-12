f6fe0512beff7ae3f228d01232318f7c6cb8e331
MeshackKipkorir <meshackkipkorir105@gmail.com>
2020-01-12 18:55:26 -0500

    saved form data to database

--- weatherApp/weather/views.py
+++ weatherApp/weather/views.py
@@ -10,7 +10,7 @@ def weather_view(request,*args,**kwargs):
     if form.is_valid():
         form.save()
         form = CityForm()
-        weatherModel.objects.create(**form.cleaned_data)
+  
 
     url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=716806f2f07ef0d6d7a6bcec3c866551"
     weather_data = []
@@ -19,9 +19,9 @@ def weather_view(request,*args,**kwargs):
         result = requests.get(url.format(city)).json()
         city_weather = {
             "city":city.name,
-            "description":result['weather'][0]['description'],
-            "temparature":result['main']['temp'],
-            "icon":result['weather'][0]['icon']
+            # "description":result['weather'][0]['description'],
+            # "temparature":result['main']['temp'],
+            # "icon":result['weather'][0]['icon']
         }
         weather_data.append(city_weather)
         
